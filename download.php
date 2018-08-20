8b3707cc4677cfd555d056fafce2bee2b1961a12
Oliver Stark <os@fortrabbit.com>
2018-08-20 15:01:55 +0200

    test full path to zcat

--- download.php
+++ download.php
@@ -7,9 +7,13 @@ $src    = $target = 'transfer.txt';
 
 echo 'DIRECTORY_SEPARATOR: ' . DIRECTORY_SEPARATOR . PHP_EOL;
 
+$find = new \Symfony\Component\Process\ExecutableFinder();
+$zcat = $find->find('zcat');
+
+
 $commands = [
-    'string_double_quotes' => 'ssh ' . $remote . ' "cat ' . $src . ' | gzip" | zcat > ' . $target,
-    'string_single_quotes' => "ssh {$remote} 'cat {$src} | gzip' | zcat > {$target}"
+    'string_double_quotes' => 'ssh ' . $remote . ' "cat ' . $src . ' | gzip" | '.$zcat.' > ' . $target,
+    'string_single_quotes' => "ssh {$remote} 'cat {$src} | gzip' | $zcat > {$target}"
 ];
 
 
